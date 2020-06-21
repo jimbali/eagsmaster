fb3e80471d2c495febde71c517b9cd556603a4ed
Jim Bailey <magicode@outlook.com>
2020-06-21 21:58:05 +0100

    refactor(js): use arrow functions, const & let, and remove semicolons

--- app/javascript/packs/edit_questions.js
+++ app/javascript/packs/edit_questions.js
@@ -1,10 +1,64 @@
-var autosaveNotification;
-var questionsConsole = document.getElementById('questions-console');
-var questionsContainer = document.getElementById('questions-root');
-var data = JSON.parse(questionsContainer.dataset.quizJson);
-var token = document.querySelector('meta[name="csrf-token"]').content;
-var baseUrl = questionsContainer.dataset.baseUrl;
-var hot = new Handsontable(questionsContainer, {
+const afterChange = (change, source) => {
+  if (source === 'loadData') return
+
+  clearTimeout(autosaveNotification)
+  row = data[change[0][0]]
+  key = change[0][1]
+  questionId = row['id']
+  fetch(
+    baseUrl + '/' + questionId,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': token
+      },
+      body: JSON.stringify(
+        { question: { title: row.title, expired: row.expired } }
+      )
+    }
+  )
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    questionsConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')'
+    autosaveNotification = setTimeout(function() {
+      questionsConsole.innerText = 'Changes will be autosaved'
+    }, 2000)
+  })
+}
+
+const beforeRemoveRow = (index, amount, physicalRows, source) => {
+  for(let i = 0; i < physicalRows.length; i++) {
+    index = physicalRows[i]
+    id = data[index].id
+    fetch(
+      baseUrl + '/' + id,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': token
+        }
+      }
+    )
+    .then((response) => {
+      questionsConsole.innerText = 'Autosaved (row deleted)'
+      autosaveNotification = setTimeout(function() {
+        questionsConsole.innerText = 'Changes will be autosaved'
+      }, 2000)
+    })
+  }
+}
+
+let autosaveNotification
+const questionsConsole = document.getElementById('questions-console')
+const questionsContainer = document.getElementById('questions-root')
+const data = JSON.parse(questionsContainer.dataset.quizJson)
+const token = document.querySelector('meta[name="csrf-token"]').content
+const baseUrl = questionsContainer.dataset.baseUrl
+const hot = new Handsontable(questionsContainer, {
   data: data,
   rowHeaders: false,
   colHeaders: ['Title', 'Finished answering?'],
@@ -16,58 +70,6 @@ var hot = new Handsontable(questionsContainer, {
   dropdownMenu: true,
   licenseKey: 'non-commercial-and-evaluation',
   contextMenu: ['remove_row'],
-  afterChange: function (change, source) {
-    if (source === 'loadData') {
-      return;
-    }
-    clearTimeout(autosaveNotification);
-    row = data[change[0][0]];
-    key = change[0][1];
-    questionId = row['id'];
-    console.log(questionId);
-    fetch(
-      baseUrl + '/' + questionId,
-      {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': token
-        },
-        body: JSON.stringify(
-          { question: { title: row.title, expired: row.expired } }
-        )
-      }
-    )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      questionsConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')';
-      autosaveNotification = setTimeout(function() {
-        questionsConsole.innerText ='Changes will be autosaved';
-      }, 2000);
-    });
-  },
-  beforeRemoveRow: function(index, amount, physicalRows, source) {
-    for(var i = 0; i < physicalRows.length; i++) {
-      index = physicalRows[i];
-      id = data[index].id;
-      fetch(
-        baseUrl + '/' + id,
-        {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': token
-          }
-        }
-      )
-      .then((response) => {
-        questionsConsole.innerText  = 'Autosaved (row deleted)';
-        autosaveNotification = setTimeout(function() {
-          questionsConsole.innerText ='Changes will be autosaved';
-        }, 2000);
-      });
-    }
-  }
-});
+  afterChange: afterChange,
+  beforeRemoveRow: beforeRemoveRow
+})
--- app/javascript/packs/edit_quiz.js
+++ app/javascript/packs/edit_quiz.js
@@ -1,11 +1,42 @@
-var quizConsole = document.getElementById('quiz-console');
-var quizContainer = document.getElementById('quiz-root');
-var data2 = JSON.parse(quizContainer.dataset.progressJson);
-var autosaveNotification2;
-var token = document.querySelector('meta[name="csrf-token"]').content;
-var updateProgressUrl = quizContainer.dataset.updateProgressUrl;
-hot2 = new Handsontable(quizContainer, {
-  data: data2,
+const afterChange = (change, source) => {
+  if (source === 'loadData') return
+
+  clearTimeout(autosaveNotification)
+
+  const row = data[change[0][0]]
+
+  fetch(
+    updateProgressUrl,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': token
+      },
+      body: JSON.stringify({ row: row })
+    }
+  )
+  .then((response) => {
+    return response.json()
+  })
+  .then((newData) => {
+    quizConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')'
+    autosaveNotification = setTimeout(function() {
+      quizConsole.innerText = 'Changes will be autosaved'
+    }, 2000)
+    data = newData
+    hot.loadData(newData)
+  })
+}
+
+const quizConsole = document.getElementById('quiz-console')
+const quizContainer = document.getElementById('quiz-root')
+let data = JSON.parse(quizContainer.dataset.progressJson)
+let autosaveNotification
+const token = document.querySelector('meta[name="csrf-token"]').content
+const updateProgressUrl = quizContainer.dataset.updateProgressUrl
+hot = new Handsontable(quizContainer, {
+  data: data,
   rowHeaders: false,
   colHeaders: JSON.parse(quizContainer.dataset.columnHeaders),
   columns: JSON.parse(quizContainer.dataset.columnData),
@@ -14,34 +45,5 @@ hot2 = new Handsontable(quizContainer, {
   licenseKey: 'non-commercial-and-evaluation',
   contextMenu: ['remove_row'],
   manualColumnResize: true,
-  afterChange: function (change, source) {
-    if (source === 'loadData') return;
-
-    clearTimeout(autosaveNotification2);
-
-    var row = data2[change[0][0]];
-
-    fetch(
-      updateProgressUrl,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': token
-        },
-        body: JSON.stringify({ row: row })
-      }
-    )
-    .then((response) => {
-      return response.json();
-    })
-    .then((newData) => {
-      quizConsole.innerText  = 'Autosaved (' + change.length + ' ' + 'cell' + (change.length > 1 ? 's' : '') + ')';
-      autosaveNotification2 = setTimeout(function() {
-        quizConsole.innerText ='Changes will be autosaved';
-      }, 2000);
-      data2 = newData;
-      hot2.loadData(newData);
-    });
-  }
-});
+  afterChange: afterChange
+})
