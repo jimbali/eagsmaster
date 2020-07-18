5ef7423d7bf3364fbe09651c98e36d28abb4a456
Jim Bailey <magicode@outlook.com>
2020-07-18 15:59:18 +0100

    test: Add more system tests for unlocking answers

--- spec/system/unlock_answer_spec.rb
+++ spec/system/unlock_answer_spec.rb
@@ -10,22 +10,29 @@ RSpec.describe 'Unlock answer', type: :system, js: true do
   before do
     quiz.update!(cursor: question.id)
 
+    player_submit_answer
+    admin_edit_points
+    player_unlock_answer
+  end
+
+  def player_submit_answer
     sign_in player
     visit quiz_question_path(quiz_id: quiz.id, id: question.id)
     fill_in 'answer', with: 'Prince Charles'
     click_on 'Submit'
+  end
 
+  def admin_edit_points
     sign_in quiz.user
     visit edit_quiz_path(id: quiz.id)
     edit_cell(question.title, '8', 1)
     page.has_text?('Autosaved (1 cell)')
+  end
 
+  def player_unlock_answer
     sign_in player
     visit quiz_question_path(quiz_id: quiz.id, id: question.id)
     click_on 'Unlock'
-
-    sign_in quiz.user
-    visit edit_quiz_path(id: quiz.id)
   end
 
   def edit_cell(heading, replacement, offset = 0)
@@ -52,11 +59,58 @@ RSpec.describe 'Unlock answer', type: :system, js: true do
     end
   end
 
-  it 'clears the answer on the leaderboard' do
-    expect(cell(question.title).text).to be_blank
+  describe 'user unlocks answer' do
+    before do
+      sign_in quiz.user
+      visit edit_quiz_path(id: quiz.id)
+    end
+
+    it 'clears the answer on the leaderboard' do
+      expect(cell(question.title).text).to be_blank
+    end
+
+    it 'clears the points on the leaderboard' do
+      expect(cell(question.title, 1).text).to be_blank
+    end
   end
 
-  it 'clears the points on the leaderboard' do
-    expect(cell(question.title, 1).text).to be_blank
+  describe 'user submits new answer' do
+    before do
+      fill_in 'answer', with: 'Charles Kennedy'
+      click_on 'Submit'
+      sign_in quiz.user
+      visit edit_quiz_path(id: quiz.id)
+    end
+
+    it 'updates the answer on the leaderboard' do
+      expect(cell(question.title).text).to eq('Charles Kennedy')
+    end
+  end
+
+  describe 'user submits new answer before edit' do
+    let(:admin) { Capybara::Session.new(:selenium) }
+
+    before do
+      Capybara.using_session(admin) do
+        sign_in quiz.user
+        visit edit_quiz_path(id: quiz.id)
+      end
+
+      fill_in 'answer', with: 'Charles Kennedy'
+      click_on 'Submit'
+
+      Capybara.using_session(admin) do
+        edit_cell(question.title, 'Ray Charles')
+      end
+    end
+
+    it 'displays an error' do
+      Capybara.using_session(admin) do
+        expect(page).to have_selector(
+          '.toast-error',
+          text: 'Did not update! Value has changed since the last refresh.'
+        )
+      end
+    end
   end
 end
